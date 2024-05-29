e7aeeaae2de7790d2171091e610eed6564be305d
PabloReca <pfernandezrecaman@gmail.com>
2024-05-29 18:06:46 +0200

    todo va

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from pynput import keyboard
+import mido
+import threading
+import time
 
+# Mapeo de notas a mensajes CC
 note_cc_mapping = {
     124: ('C', 1),  # Originalmente 105
     127: ('Db', 15),  # Originalmente 106
@@ -21,6 +25,23 @@ alt_note_cc_mapping = {
 
 current_modifiers = set()
 
+# Listar puertos MIDI disponibles
+print("Puertos MIDI disponibles:")
+for port in mido.get_output_names():
+    print(port)
+
+# Configurar el puerto MIDI
+try:
+    midi_out = mido.open_output('K2 1')
+    print("Puerto MIDI abierto con éxito.")
+except IOError:
+    print("Error al abrir el puerto MIDI. Asegúrate de que el nombre del puerto es correcto.")
+
+def send_midi_cc(value):
+    # Aseguramos que el mensaje se envía en el canal 1
+    cc_message = mido.Message('control_change', channel=0, control=20, value=value)  # Canal 0 es el canal 1 en MIDI
+    midi_out.send(cc_message)
+    print(f"Mensaje MIDI enviado: CC {value}")
 
 def get_note_and_modifiers(key_code):
     if 'Alt' in current_modifiers:
@@ -28,34 +49,26 @@ def get_note_and_modifiers(key_code):
     else:
         return note_cc_mapping.get(key_code, ('Unknown Note', None))
 
-
 def show_popup(note_name):
-    # Crear la ventana emergente
     popup = tk.Toplevel()
     popup.overrideredirect(True)  # Sin bordes ni botones
     popup.configure(bg='red')
 
-    # Obtener el tamaño de la pantalla
     screen_width = popup.winfo_screenwidth()
     screen_height = popup.winfo_screenheight()
 
-    # Tamaño de la ventana emergente
     window_width = 300
     window_height = 100
 
-    # Calcular la posición de la ventana emergente
     x = (screen_width - window_width) / 2
     y = (screen_height - window_height) / 2
 
     popup.geometry(f'{window_width}x{window_height}+{int(x)}+{int(y)}')
 
-    # Crear una etiqueta con el texto
     label = tk.Label(popup, text=f'KEY: {note_name}', font=('Helvetica', 24), fg='white', bg='red')
     label.pack(expand=True)
 
-    # Ocultar la ventana emergente después de 1 segundo
     popup.after(1000, popup.destroy)
-
 
 def on_press(key):
     try:
@@ -65,20 +78,18 @@ def on_press(key):
         note_tuple = get_note_and_modifiers(key_code)
         if note_tuple[1] is not None:
             print(f"Key pressed: {key_code} {note_tuple[0]} {note_tuple[1]}")
+            send_midi_cc(note_tuple[1])
             show_popup(note_tuple[0])
         else:
             print(f"Unknown key pressed: {key_code}")
     except AttributeError:
         print(f'Special key pressed: {key}')
 
-
 def on_release(key):
     if key == keyboard.Key.alt_l or key == keyboard.Key.alt_r:
         current_modifiers.discard('Alt')
     if key == keyboard.Key.esc:
-        # Stop listener
         return False
-
 
 # Configurar el listener del teclado
 listener = keyboard.Listener(
